81c01f9cd827dd6f57068d36f779fe5120e6b9fc
Artem Altukhov <aaltukhov@acumatica.com>
2017-11-16 20:16:34 +0300

    second commit

--- Indexer.cs
+++ Indexer.cs
@@ -6,5 +6,17 @@ using System.Threading.Tasks;
 
 namespace Incapsulation.Weights
 {
-	
+    class Indexer
+    {
+        readonly double[] originalArray;
+        public double[] SubArray { get; private set; }
+
+        public Indexer(double[] array, int start, int length)
+        {
+            this.originalArray = array;
+            SubArray = new double[length - start];
+            Array.Copy(originalArray, start, SubArray, 0, length);
+        }
+
+    }
 }
