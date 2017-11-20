02106035574a514032d6fcad349355dfa4e81f24
Artem Altukhov <altukhov.artem.dev@gmail.com>
2017-11-20 23:38:49 +0300

    third commit

--- Indexer.cs
+++ Indexer.cs
@@ -6,15 +6,48 @@ using System.Threading.Tasks;
 
 namespace Incapsulation.Weights
 {
-    class Indexer
+    struct Indexer
     {
-        readonly double[] originalArray;
+        private readonly double[] originalArray;
         public double[] SubArray { get; private set; }
+        
+        public int Length
+        {
+            get { return SubArray.Length; }
+        }
+
+        public double this[int index]
+        {
+            get
+            {
+                if (index < 0 || index > SubArray.Length)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                return SubArray[index];
+            }
+            set
+            {
+                if (index < 0 || index > SubArray.Length)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                SubArray[index] = value;
+            }
+        }
+
+         
+
+
 
         public Indexer(double[] array, int start, int length)
         {
+            if (start < 0 || start > array.Length || length < 0 || length > array.Length - start)
+            {
+                throw new ArgumentException();
+            }
             this.originalArray = array;
-            SubArray = new double[length - start];
+            SubArray = new double[length];
             Array.Copy(originalArray, start, SubArray, 0, length);
         }
 
--- Program.cs
+++ Program.cs
@@ -1,11 +1,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using NUnitLite;
+using Incapsulation.Weights;
 
 class Program
 {
 	static void Main(string[] args)
 	{
-		new AutoRun().Execute(args);
+        double[] array = new double[] { 1, 2, 3, 4 };
+        var indexer1 = new Indexer(array, 1, 2);
+        var indexer2 = new Indexer(array, 0, 2);
+        indexer1[0] = 100500;
+        System.Console.WriteLine(indexer2[0]);
+
+        new AutoRun().Execute(args);
 	}
 }
